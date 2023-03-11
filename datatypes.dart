441d2103586c51d7380b502d838a16a75eafdfbe
Refilwe_Gololo <94147480+Fifi7@users.noreply.github.com>
2023-03-11 14:17:31 +0200

    Add files via upload

---
+++ datatypes.dart
@@ -0,0 +1,36 @@
+// The following program shows the different DataTypes in Dart Programming:
+
+// Boolean Datatype:
+// This is a program that checks if 20 is greater than 10, then it returns True.
+void main() {
+  bool isTrue;
+  isTrue = 20 > 10;
+  print(isTrue);
+
+// This program checks if 23 is less than 21 then it will return False:
+  bool myAge;
+  myAge = 23 < 21;
+  print(myAge);
+
+// The following program prints out my name and surname. It shows the String datatype:
+  String myName = "Refilwe";
+  String mySurname = "Gololo";
+  print(myName);
+  print(mySurname);
+
+// The following program shows the double datatype:
+  double averageHeight = 14.7;
+  print(averageHeight);
+
+// The following program shows the Integer Datataype:
+  int CurrentAge = 23;
+  print(CurrentAge);
+
+// The following program shows the List DataType:
+  List myDreamCars = ["BMWi8", "Lambhorghini"];
+  print(myDreamCars);
+
+//The following program shows the runes DataType:
+  Runes input = new Runes(' \u{1f605} ');
+  print(new String.fromCharCodes(input));
+}
